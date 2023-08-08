5a72ec6d323a7d6ff65560ccd0ffd4f572a0757e
Hill-Man-1 <hilmaaansf@gmail.com>
2023-08-08 21:33:19 +0700

    try again

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.get('/', (req, res) => {
                 id: 1,
                 title: 'Title 1'
             }
-        ]
-    });
-}); 
+        ] 
+    });  
+});  
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
