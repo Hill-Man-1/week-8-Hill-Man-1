f5c6231c53d588d3b44c00dc044ea42c27fd0b72
Hill-Man-1 <hilmaaansf@gmail.com>
2023-08-09 22:59:34 +0700

    fix typescript and package.json

---
+++ data.ts
@@ -0,0 +1,104 @@
+import express, {Response,Request} from 'express';
+import * as dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+interface ICashflow{
+    id:number;
+    cashflow:string;
+    description:string;
+    amount:number;
+    note:string;
+}
+
+let cashFlows: ICashflow[] = [
+    {
+    id: 1,
+    cashflow: "Cash In",
+    description: "Salary",
+    amount: 10000,
+    note: "Salary"},
+];
+
+app.get('/cashflow', (req: express.Request, res: express.Response) => {
+    res.json(cashFlows);
+});  
+
+app.get('/cashflow/:id', (req: express.Request, res: express.Response) => {
+    const id = parseInt(req.params.id);
+    const cashflow = cashFlows.find((p) => p.id === id);
+    if(cashflow){
+        res.json({cashflow});
+    }else{
+        res.status(404).json({message:'Cashflow not found'});
+    }
+});
+
+app.post('/cashflow', (req: express.Request, res: express.Response) => {
+    const newCashflow: ICashflow = {
+        id: cashFlows.length + 1,
+        cashflow: req.body.cashflow,
+        description: req.body.description,
+        amount: req.body.amount,
+        note: req.body.note,
+    };
+    cashFlows.push(newCashflow);
+    res.status(200).json(newCashflow);
+})
+
+app.put('/cashflow/:id', (req: express.Request, res: express.Response) => {
+    const id = parseInt(req.params.id);
+    const cashFlowIndex = cashFlows.findIndex((p) => p.id === id);
+    if (cashFlowIndex !== -1){
+        const updateCashFlow : ICashflow = {
+            id,
+            cashflow: req.body.cashflow,
+            description: req.body.description,
+            amount: req.body.amount,
+            note: req.body.note,
+        };
+        cashFlows[cashFlowIndex] = updateCashFlow;
+        res.json(updateCashFlow);
+    }else{
+        res.status(404).json({message: "Cash Flow Not Found"})
+    }
+});
+
+app.patch('/cashflow/:id', (req: express.Request, res: express.Response) => {
+    const id = parseInt(req.params.id);
+    const cashFlowIndex = cashFlows.findIndex((p) => p.id === id);
+    if (cashFlowIndex !== -1) {
+        const updateCashFlow: ICashflow = {
+        ...cashFlows[cashFlowIndex],
+        ...req.body,
+        };
+        cashFlows[cashFlowIndex] = updateCashFlow;
+        res.json(updateCashFlow);
+    } else {
+        res.status(404).json({message: "Cash Flow Not Found"})
+    }
+});
+
+app.delete('/cashflow/:id', (req: express.Request, res: express.Response) =>{
+    const id = parseInt(req.params.id);
+    const cashFlowIndex = cashFlows.findIndex((p) => p.id === id);
+    if (cashFlowIndex !== -1) {
+        const deleteCashFlow = cashFlows.findIndex((p) => p.id === id);
+        if (cashFlowIndex !==-1){
+            const deleteCashFlow = cashFlows.splice(cashFlowIndex,1)[0];
+            res.json(deleteCashFlow);
+        } else {
+            res.status(404).json({message: "Cash Flow Not Found"})
+        }
+    }
+});
